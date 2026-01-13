d6aa985f7069af9985715f4f009e7f085b05ca9b
NohamDIIAGE <noham.gaba@diiage.org>
2026-01-13 15:55:30 +0100

    feature : return a dto and not a object

---
+++ Statly.API/Dtos/ClubDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Statly.API.Dtos
+{
+    // DTO de sortie = ce que l'API RENVOIE au client
+    public class ClubDto
+    {
+        // Identifiant du club (côté API)
+        public int Id { get; set; }
+
+        // Nom du club
+        public string Name { get; set; } = null!;
+
+        // Pays du club
+        public string Country { get; set; } = null!;
+
+        // Logo du club
+        public string LogoUrl { get; set; } = null!;
+    }
+}
